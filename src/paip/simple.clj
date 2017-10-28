dc33258a889de23b1cbdebe364035fc50a0a088d
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-10-28 05:36:05 +0200

    Adding random generation of sentences.

--- src/paip/simple.clj
+++ src/paip/simple.clj
@@ -1,6 +1,25 @@
 (ns paip.simple)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn random-elt [choices]
+  "Choose an element from a list at random."
+  (nth choices (rand-int (count choices))))
+
+(defn one-of [set]
+  "Pick one element of set, and make a list of it"
+  (list (random-elt set)))
+
+(defn verb [] (one-of '(hit took saw like)))
+
+(defn noun [] (one-of '(man ball woman table)))
+
+(defn article [] (one-of '(the a)))
+
+(defn noun-phrase [] (concat (article) (noun)))
+
+(defn verb-phrase [] (concat (verb) (noun-phrase)))
+
+(defn sentence []  (concat (noun-phrase) (verb-phrase)))
+
+
+
+
