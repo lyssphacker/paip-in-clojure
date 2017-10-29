70811e1e64b5b117505931fdf35b98f7f7eda537
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-10-29 10:15:09 +0100

    Fixing project definition.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject paip-in-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "PAIP in Clojure"
+  :url "https://github.com/lyssphacker/paip-in-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
