f7b4b20b961eebabc32975a581a6d5a334721da2
Hrvoje Krot <hkrot@infoartgroup.com>
2017-11-02 17:01:20 +0100

    Using map to represent operation.

--- src/paip/gps1.clj
+++ src/paip/gps1.clj
@@ -1,10 +1,17 @@
 (ns ^{:doc "First version of GPS (General Problem Solver)"}
-  paip.gps1)
+paip.gps1)
 
 (def state
-  "The current state: a list of conditions."
+  "The current state: a set of conditions."
   #{})
 
 (def ops
-  "A list of available operators."
+  "A set of available operators."
   #{})
+
+(def op
+  "An operation"
+  {:action   nil
+   :preconds #{}
+   :add-set  #{}
+   :del-set  #{}})
