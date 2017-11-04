85916b9372c23165f76b000d6253a9d5cfc8cc65
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-04 21:12:12 +0100

    Adding executing? predicate.

--- src/paip/gps.clj
+++ src/paip/gps.clj
@@ -1,3 +1,12 @@
 (ns ^{:doc "Final version of GPS"}
 paip.gps
-  (:require [paip.gps1 :refer :all]))
+  (:require [paip.gps1 :refer :all]
+            [clojure.string :as str]))
+
+(defn executing?
+  "Is x of the form: (executing ...) ?"
+  [x]
+  (str/starts-with? x "executing"))
+
+
+
