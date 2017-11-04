70c25df5c1d706dd3dc764deff5fceeac71a5b69
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-04 21:56:15 +0100

    Adding executing? and convert-op.

--- src/paip/gps.clj
+++ src/paip/gps.clj
@@ -1,12 +1,29 @@
 (ns ^{:doc "Final version of GPS"}
 paip.gps
-  (:require [paip.gps1 :refer :all]))
+  (:require [paip.gps1 :refer :all]
+            [clojure.inspector :refer :all]))
 
 (defn starts-with
   "Is this a list whose first element is x?"
   [list x]
   (and (not (atom? list))
        (= (first list) x)))
+
+(defn executing?
+  "Is x of the form: (executing ...) ?"
+  [x]
+  (starts-with x 'executing))
+
+(defn convert-op
+  "Make op conform to the (EXECUTING op) convention."
+  [op]
+  (if (some executing? (:add-set op))
+    op
+    (update op :add-set
+            (fn [add-set]
+              (cons
+                (list 'executing (:action op))
+                add-set)))))
 
 
 
