da3a2fbdf89f6a3c58f24e9f546971d9e15c70ee
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-04 05:35:10 +0100

    Adding another test.

--- test/paip/gps1-test.clj
+++ test/paip/gps1-test.clj
@@ -2,10 +2,17 @@
   (:require [clojure.test :refer :all]
             [paip.gps1 :refer :all]))
 
-(deftest gps-test
-  (testing "gps"
+(deftest gps-test-1
+  (testing "gps-1"
     (is (= 'solved (gps #{'son-at-home 'car-needs-battery
                           'have-money 'have-phone-book}
+                        #{'son-at-school}
+                        school-ops)))))
+
+(deftest gps-test-2
+  (testing "gps-2"
+    (is (= nil (gps #{'son-at-home 'car-needs-battery
+                          'have-money}
                         #{'son-at-school}
                         school-ops)))))
 
