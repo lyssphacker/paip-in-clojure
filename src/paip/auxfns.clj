9314b2adb710a1f448de05cfc1a6da6246a32d36
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-05 15:27:21 +0100

    Adding find-all.

--- src/paip/auxfns.clj
+++ src/paip/auxfns.clj
@@ -1,7 +1,18 @@
 (ns ^{:doc "Auxiliary functions used by all other programs"}
-paip.auxfns)
+  paip.auxfns)
 
 (defn mappend
   "Append the results of calling fn on each element of list."
   [fn list]
   (apply concat (map fn list)))
+
+(defn appropriate?
+  "An op is appropriate to a goal if it is in its add list."
+  [goal]
+  (fn [op]
+    (contains? (:add-set op) goal)))
+
+(defn find-all
+  "Find all those elements of sequence that match item"
+  [goal ops]
+  (filter (appropriate? goal) ops))
