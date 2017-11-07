6396967494fc15374c3f3a2de50dc713aae334c4
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-07 05:14:52 +0100

    Code reformat for easier debugging.

--- src/paip/gps.clj
+++ src/paip/gps.clj
@@ -30,46 +30,54 @@ paip.gps
 (defn apply-op
   "Return a new, transformed state if op is applicable."
   [state goal op goal-stack ops]
-  (let [state2 (achieve-all state (:preconds op)
-                            (cons goal goal-stack) ops)]
-    (when-not (empty? state2)
-      (union (filter (complement
-                       (fn [x]
-                         (in? x (:del-vec op))))
-                     state2)
-             (:add-vec op)))))
+  (let [state2
+        (achieve-all state (:preconds op)
+                     (cons goal goal-stack) ops)]
+    (when-not
+      (empty? state2)
+      (union
+        (filter
+          (complement
+            (fn [x]
+              (in? x (:del-vec op))))
+          state2)
+        (:add-vec op)))))
 
 (defn achieve
   "A goal is achieved if it already holds,
   or if there is an appropriate op for it that is applicable."
   [state goal goal-stack ops]
-  (cond (in? goal state) state
-        (in? goal goal-stack) '()
-        :else (some
-                (fn [op]
-                  (apply-op state goal op goal-stack ops))
-                (find-all goal ops))))
+  (cond
+    (in? goal state) state
+    (in? goal goal-stack) '()
+    :else (some
+            (fn [op]
+              (apply-op state goal op goal-stack ops))
+            (find-all goal ops))))
 
 (defn achieve-all
   "Achieve each goal, and make sure they still hold at the end."
   [state goals goal-stack ops]
   (with-local-vars [current-state state]
-    (if (and (every?
-               (fn [g]
-                 (var-set current-state
-                          (achieve state g goal-stack ops)))
-               goals)
-             (subset? (set goals) (set @current-state)))
+    (if
+      (and
+        (every?
+          (fn [g]
+            (var-set current-state
+                     (achieve state g goal-stack ops)))
+          goals)
+        (subset?
+          (set goals)
+          (set @current-state)))
       @current-state)))
 
 (defn gps
   "General Problem Solver: from state, achieve goals using ops."
   [state goals ops]
-  (filter (complement atom?)
-          (achieve-all (cons 'start state)
-                       goals
-                       []
-                       ops)))
+  (achieve-all (cons 'start state)
+               goals
+               []
+               ops))
 
 
 
