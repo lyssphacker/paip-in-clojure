6dc9a8cff973e98afd3ef5c39f7b1779ae022b31
Hrvoje Krot <hkrot@infoartgroup.com>
2017-11-07 07:38:37 +0100

    Adding tests for gps function.

--- test/paip/gps-test.clj
+++ test/paip/gps-test.clj
@@ -4,7 +4,52 @@
 
 (deftest gps-test-1
   (testing "gps-1"
-    (is (= 'solved (gps ['son-at-home 'car-needs-battery
-                         'have-money 'have-phone-book]
-                        ['son-at-school]
-                        converted-school-ops)))))
+    (is (= '(start
+              executing-look-up-number
+              executing-telephone-shop
+              executing-tell-shop-problem
+              executing-give-shop-money
+              executing-shop-installs-battery
+              executing-drive-son-to-school)
+           (gps ['son-at-home 'car-needs-battery
+                 'have-money 'have-phone-book]
+                ['son-at-school]
+                converted-school-ops)))))
+
+(deftest gps-test-2
+  (testing "gps-2"
+    (is (= '(start
+              executing-drive-son-to-school)
+           (gps ['son-at-home 'car-works]
+                ['son-at-school]
+                converted-school-ops)))))
+
+(deftest gps-test-3
+  (testing "gps-3"
+    (is (= '()
+           (gps ['son-at-home 'car-needs-battery
+                 'have-money 'have-phone-book]
+                ['have-money 'son-at-school]
+                converted-school-ops)))))
+
+(deftest gps-test-4
+  (testing "gps-4"
+    (is (= '()
+           (gps ['son-at-home 'car-needs-battery
+                 'have-money 'have-phone-book]
+                ['son-at-school 'have-money]
+                converted-school-ops)))))
+
+(deftest gps-test-5
+  (testing "gps-5"
+    (is (= '()
+           (gps ['son-at-home 'car-needs-battery 'have-money]
+                ['son-at-school]
+                converted-school-ops)))))
+
+(deftest gps-test-6
+  (testing "gps-6"
+    (is (= '(start)
+           (gps ['son-at-home]
+                ['son-at-home]
+                converted-school-ops)))))
