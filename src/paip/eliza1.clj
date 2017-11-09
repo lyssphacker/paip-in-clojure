3393837cde43ac13a9eb97e0d10c1f871a0db3b3
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-09 20:47:41 +0100

    Adding pat-match.

--- src/paip/eliza1.clj
+++ src/paip/eliza1.clj
@@ -1,2 +1,20 @@
-(ns paip.eliza1)
+(ns ^{:doc "Basic version of the Eliza program"}
+  paip.eliza1
+  (:require [clojure.inspector :refer (atom?)]
+            [paip.auxfns :refer (match-variable no-bindings fail variable? cons?)]))
 
+(defn pat-match
+  ([pattern input]
+   (pat-match pattern input no-bindings))
+  ([pattern input bindings]
+   (cond (= bindings fail) fail
+         (variable? pattern) (match-variable pattern input bindings)
+         (= pattern input) bindings
+         (and
+           (cons? pattern)
+           (cons? input)) (pat-match (rest pattern)
+                                     (rest input)
+                                     (pat-match (first pattern)
+                                                (first input)
+                                                bindings))
+         :else fail)))
