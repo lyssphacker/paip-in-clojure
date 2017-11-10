b75b237e75ba2c0247a7a713f767cf6c65e31de9
Hrvoje Krot <hkrot@infoartgroup.com>
2017-11-10 08:04:25 +0100

    Adding segment-match.

--- src/paip/eliza1.clj
+++ src/paip/eliza1.clj
@@ -1,13 +1,36 @@
 (ns ^{:doc "Basic version of the Eliza program"}
-  paip.eliza1
+paip.eliza1
   (:require [clojure.inspector :refer (atom?)]
-            [clojure.string :refer (starts-with?)]
-            [paip.auxfns :refer (match-variable no-bindings fail variable? cons?)]))
+            [paip.auxfns :refer (match-variable no-bindings fail variable?
+                                                cons? position starts-with)]))
 
 (defn segment-pattern?
+  "Is this a segment matching pattern: ((?* var) . pat)"
   [pattern]
   (and (cons? pattern)
-       (starts-with? (first pattern) "?*")))
+       (starts-with (first pattern) '?*)))
+
+(declare pat-match)
+
+(defn segment-match
+  ([pattern input bindings]
+   (segment-match pattern input bindings 0))
+  ([pattern input bindings start]
+   (let [var (second (first pattern))
+         pat (rest pattern)]
+     (if (empty? pat)
+       (match-variable var input bindings)
+       ;; We assume that pat starts with a constant
+       ;; In other words, a pattern can't have 2 consecutive vars
+       (let [pos (position input (first pat) start)]
+         (if (= -1 pos)
+           fail
+           (let [b2 (pat-match pat (drop pos input) bindings)]
+             ;; If this match failed, try another longer one
+             ;; If it worked, check that the variables match
+             (if (= b2 fail)
+               (segment-match pattern input bindings (+ pos 1))
+               (match-variable var (take pos input) b2)))))))))
 
 (defn pat-match
   ([pattern input]
@@ -16,6 +39,7 @@
    (cond (= bindings fail) fail
          (variable? pattern) (match-variable pattern input bindings)
          (= pattern input) bindings
+         (segment-pattern? pattern) (segment-match pattern input bindings)
          (and
            (cons? pattern)
            (cons? input)) (pat-match (rest pattern)
