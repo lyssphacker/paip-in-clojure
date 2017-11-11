e4ad5cad74f98af1490336126a1f1d1952b94267
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-11 18:13:17 +0100

    Finishing advanced Eliza implementation.

--- src/paip/eliza.clj
+++ src/paip/eliza.clj
@@ -1,8 +1,8 @@
 (ns ^{:doc "Advanced version of Eliza.
 Has more rules, and accepts input without parens."}
 paip.eliza
-  (:require [paip.eliza1 :refer :all]
-            [clojure.string :refer (replace)]))
+  (:require [paip.eliza1 :refer (use-eliza-rules)]
+            [clojure.pprint :refer (cl-format pprint)]))
 
 (defn read-line-no-punct
   "Read an input line, ignoring punctuation."
@@ -15,3 +15,21 @@ paip.eliza
         #"[-.,;:`!?#()\"]"
         " ")
       ")")))
+
+(defn print-with-spaces
+  [list]
+  (cl-format true "~{~a ~}~%" list))
+
+(defn eliza
+  "Respond to user input using pattern matching rules."
+  []
+  (loop []
+    (println 'eliza>)
+    (let [input (read-line-no-punct)]
+      (if (= input '(quit))
+        'done
+        (do
+          (print-with-spaces
+            (flatten
+              (use-eliza-rules input)))
+          (recur))))))
