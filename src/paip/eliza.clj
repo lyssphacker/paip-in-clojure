959a6a3893c192ce22c8e7667e9afd023e77ff42
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-11 17:42:15 +0100

    Adding read-line-no-punct.

--- src/paip/eliza.clj
+++ src/paip/eliza.clj
@@ -1,6 +1,17 @@
 (ns ^{:doc "Advanced version of Eliza.
 Has more rules, and accepts input without parens."}
 paip.eliza
-  (:require [paip.eliza1 :refer :all]))
+  (:require [paip.eliza1 :refer :all]
+            [clojure.string :refer (replace)]))
 
-
+(defn read-line-no-punct
+  "Read an input line, ignoring punctuation."
+  []
+  (read-string
+    (str
+      "("
+      (clojure.string/replace
+        (read-line)
+        #"[-.,;:`!?#()\"]"
+        " ")
+      ")")))
