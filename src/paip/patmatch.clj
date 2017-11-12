1a9ff95e9a5e46a69174939b43a68d1e35056bce
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-12 16:01:03 +0100

    Implementing pattern matching tool.

--- src/paip/patmatch.clj
+++ src/paip/patmatch.clj
@@ -2,11 +2,47 @@
 paip.patmatch
   (:require [paip.auxfns :refer (variable? fail no-bindings
                                            match-variable cons?)]))
-(declare segment-pattern?)
+(declare segment-match)
+(defn segment-match
+  [pattern input bindings]
+  input)
 
-(declare segment-matcher)
+(def pattern-fn-map
+  {'?is  'match-is,
+   '?or  'match-or,
+   '?and 'match-and,
+   '?not 'match-not,
+   '?*   'segment-match,
+   '?+   'segment-match+,
+   '??   'segment-match?,
+   '?if  'match-if})
 
-(declare single-pattern?)
+(defn match-fn
+  "Get the matching function for x,
+  if it is a symbol that has one."
+  [x]
+  (when
+    (symbol? x)
+    (resolve (x pattern-fn-map))))
+
+(defn segment-pattern?
+  "Is this a segment-matching pattern like ((?* var) . pat)?"
+  [pattern]
+  (and (cons? pattern) (cons? (first pattern))
+       (symbol? (first (first pattern)))
+       (not (nil? (match-fn (first (first pattern)))))))
+
+(defn single-pattern?
+  "Is this a single-matching pattern?
+  E.g. (?is x predicate) (?and . patterns) (?or . patterns)."
+  [pattern]
+  (and (cons? pattern)
+       (not (nil? (match-fn (first pattern))))))
+
+(defn segment-matcher
+  [pattern input bindings]
+  ((match-fn (first (first pattern)))
+    pattern input bindings))
 
 (declare single-matcher)
 
