9b15061c72f1a8bfcec10acaaca5d5394c597b13
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-12 12:50:48 +0100

    Defining skeleton for pat-match.

--- src/paip/patmatch.clj
+++ src/paip/patmatch.clj
@@ -1,2 +1,29 @@
 (ns ^{:doc "Pattern matcher from section 6.2"}
-  paip.patmatch)
+paip.patmatch
+  (:require [paip.auxfns :refer (variable? fail no-bindings
+                                           match-variable cons?)]))
+(declare segment-pattern?)
+
+(declare segment-matcher)
+
+(declare single-pattern?)
+
+(declare single-matcher)
+
+(defn pat-match
+  ([pattern input]
+   (pat-match pattern input no-bindings))
+  ([pattern input bindings]
+   (cond (= bindings fail) fail
+         (variable? pattern) (match-variable pattern input bindings)
+         (= pattern input) bindings
+         (segment-pattern? pattern) (segment-matcher pattern input bindings)
+         (single-pattern? pattern) (single-matcher pattern input bindings)
+         (and
+           (cons? pattern)
+           (cons? input)) (pat-match (rest pattern)
+                                     (rest input)
+                                     (pat-match (first pattern)
+                                                (first input)
+                                                bindings))
+         :else fail)))
