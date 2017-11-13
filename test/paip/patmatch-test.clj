da15c6a9bd64116bac166d9b6ef242d8895e8fce
Hrvoje Krot <hkrot@infoartgroup.com>
2017-11-13 07:47:13 +0100

    Renaming tests.

--- test/paip/patmatch-test.clj
+++ test/paip/patmatch-test.clj
@@ -3,14 +3,14 @@
             [paip.patmatch :refer :all]))
 
 (deftest pat-match-test-1
-  (testing "pat-match"
+  (testing "pat-match-1"
     (is (= {'?y '(d), '?x '(b c)}
            (pat-match
              '(a (?* ?x) (?* ?y) ?x ?y)
              '(a b c d (b c) (d)))))))
 
 (deftest pat-match-test-2
-  (testing "pat-match"
+  (testing "pat-match-2"
     (is (= nil
            (pat-match
              '(?x ?op ?y (?if (?op ?x ?y)))
