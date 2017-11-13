c94bbb3e83266adb8e7ce33e129363e1bb962973
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-13 05:30:30 +0100

    Adding test for pat-match.

--- test/paip/patmatch-test.clj
+++ test/paip/patmatch-test.clj
@@ -2,9 +2,16 @@
   (:require [clojure.test :refer :all]
             [paip.patmatch :refer :all]))
 
-(deftest pat-match-test
-  (testing "pat-match"
+(deftest pat-match-test-1
+  (testing "pat-match-1"
     (is (= {'?y '(d), '?x '(b c)}
            (pat-match
              '(a (?* ?x) (?* ?y) ?x ?y)
              '(a b c d (b c) (d)))))))
+
+(deftest pat-match-test-2
+  (testing "pat-match-2"
+    (is (= {'?x 3, '?op '+, '?y 4, '?z 7}
+           (pat-match
+             '(?x ?op ?y is ?z (?if (= (?op ?x ?y) ?z)))
+             '(3 + 4 is 7))))))
