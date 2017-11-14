050e0129105252fa1f1704b9d71b561c8c8bffb1
Hrvoje Krot <hkrot@infoartgroup.com>
2017-11-14 16:27:12 +0100

    Adding tree-search.

--- src/paip/search.clj
+++ src/paip/search.clj
@@ -1,2 +1,17 @@
 (ns ^{:doc "Search routines from section 6.4"}
-  paip.search)
+  paip.search
+  (:require [paip.auxfns :refer (funcall fail)]))
+
+(defn tree-search
+  "Find a state that satisfies goal-p.  Start with states,
+  and search according to successors and combiner."
+  [states goal? successors combiner]
+  (cond
+    (empty? states) fail
+    (funcall goal? (first states)) (first states)
+    :else (tree-search
+            (funcall combiner
+                     (funcall successors (first states)
+                              (rest states)))
+            goal? successors combiner)))
+
