1d088d0500bdbe867d127ac3e5449aeaf40d2922
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-18 18:35:03 +0100

    Adding partial implementation of Student.

--- src/paip/student.clj
+++ src/paip/student.clj
@@ -1,16 +1,19 @@
 (ns ^{:doc "Chapter 7's STUDENT program to solve algebra word problems."}
 paip.student
-  (:require [paip.patmatch :refer :all]
-            [paip.auxfns :refer (fmap)]))
+  (:require [paip.patmatch :refer (pat-match expand-pat-match-abbrev
+                                             rule-based-translator)]
+            [paip.auxfns :refer (fmap remove-if fmap-values in?)]
+            [clojure.walk :refer (postwalk-replace)]
+            [clojure.inspector :refer (atom?)]))
 
 
 (def student-rules-abbrev
   {'(?x* .)                          '?x,
    '(?x* . ?y*)                      '(?x ?y),
-   '(if ?x* \, then ?y*)               '(?x ?y),
+   '(if ?x* \, then ?y*)             '(?x ?y),
    '(if ?x* then ?y*)                '(?x ?y),
-   '(if ?x* \, ?y*)                    '(?x ?y),
-   '(?x* \, and ?y*)                   '(?x ?y),
+   '(if ?x* \, ?y*)                  '(?x ?y),
+   '(?x* \, and ?y*)                 '(?x ?y),
    '(find ?x* and ?y*)               '((= to-find-1 ?x) (= to-find-2 ?y)),
    '(find ?x*)                       '(= to-find ?x),
    '(?x* equals ?y*)                 '(= ?x ?y),
@@ -43,3 +46,61 @@ paip.student
   (fmap
     student-rules-abbrev
     expand-pat-match-abbrev))
+
+(declare solve-equations)
+
+(defn rule-pattern [rule] (get 0 rule))
+(defn rule-response [rule] (get 1 rule))
+
+(declare translate-to-expression)
+
+(defn translate-pair
+  "Translate the value part of the pair into an equation or expression."
+  [bindings]
+  (fmap-values bindings translate-to-expression))
+
+(defn make-variable
+  "Create a variable name based on the given list of words"
+  [words]
+  (first words))
+
+(defn translate-to-expression
+  "Translate an English phrase into an equation or expression."
+  [words]
+  (or
+    (rule-based-translator
+      words
+      student-rules
+      pat-match
+      rule-pattern
+      rule-response
+      (fn
+        [bindings response]
+        (postwalk-replace
+          (translate-pair bindings)
+          response)))
+    (make-variable words)))
+
+(defn noise-words?
+  "Is this a low-content word which can be safely ignored?"
+  [word]
+  (in? word '(a an the this number of $)))
+
+(defn create-list-of-equations
+  "Separate out equations embedded in nested parens."
+  [exp]
+  (cond (empty? exp) '()
+        (atom? (first exp)) (list exp)
+        :else (concat
+                (create-list-of-equations (first exp))
+                (create-list-of-equations (rest exp)))))
+
+(defn student
+  "Solve certain Algebra Word Problems."
+  [words]
+  (create-list-of-equations
+    (translate-to-expression
+      (remove-if
+        noise-words?
+        words))))
+
