466272da26dacf75903985466a24c8934b61130f
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-12-02 06:27:43 +0100

    Adding dependencies.

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.trace "0.7.9"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [better-cond "1.0.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]])
