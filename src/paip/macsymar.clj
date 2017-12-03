516d96ab2a66b8733ba0f8f15af4892280bb1867
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-12-03 06:14:38 +0100

    Adding simplification rules.

---
+++ src/paip/macsymar.clj
@@ -0,0 +1,36 @@
+(ns ^{:doc "The rewrite rules for MACSYMA"}
+paip.macsymar
+  (:require
+    [paip.macsyma :refer (infix->prefix)]))
+
+(def simplification-rules
+  (map infix->prefix
+       '((x + 0 = x)
+          (0 + x = x)
+          (x + x = 2 * x)
+          (x - 0 = x)
+          (0 - x = - x)
+          (x - x = 0)
+          (- - x = x)
+          (x * 1 = x)
+          (1 * x = x)
+          (x * 0 = 0)
+          (0 * x = 0)
+          (x * x = x expt 2)
+          (x / 0 = undefined)
+          (0 / x = 0)
+          (x / 1 = x)
+          (x / x = 1)
+          (0 expt 0 = undefined)
+          (x expt 0 = 1)
+          (0 expt x = 0)
+          (1 expt x = 1)
+          (x expt 1 = x)
+          (x expt -1 = 1 / x)
+          (x * (y / x) = y)
+          ((y / x) * x = y)
+          ((y * x) / x = y)
+          ((x * y) / x = y)
+          (x + - x = 0)
+          ((- x) + x = 0)
+          (x + y - x = y))))
