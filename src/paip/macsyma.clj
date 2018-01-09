3b96b2e66f0e1642f5d9ab123d4f3c54e16f0833
Hrvoje Krot <hrvoje.krot@gmail.com>
2018-01-09 06:00:43 +0100

    Not using namespace in symbol name.

--- src/paip/macsyma.clj
+++ src/paip/macsyma.clj
@@ -315,7 +315,7 @@ paip.macsyma
 
 (defn deriv-divides
   [factor factors x]
-  (assert (starts-with factor 'paip.macsyma/expt))
+  (assert (starts-with factor 'expt))
   (let [u (exp-lhs factor)
         n (exp-rhs factor)
         k (divide-factors
