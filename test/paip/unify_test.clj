46209e5c8bf0832364a5ac01d52f170bdc8b5d2b
Hrvoje Krot <hrvoje.krot@gmail.com>
2018-01-10 20:09:45 +0100

    Adding another test for unify.

--- test/paip/unify_test.clj
+++ test/paip/unify_test.clj
@@ -6,3 +6,8 @@
   (testing "unify-1"
     (is (= {'?x '?y}
            (unify '(f ?x) '(f ?y))))))
+
+(deftest unify-test-2
+  (testing "unify-2"
+    (is (= {'?a '?x, '?x '?y, '?y 0}
+           (unify '(?a + ?a = 0) '(?x + ?y = ?y))))))
