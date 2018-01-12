6e8faaf4cf0d55ddac8d39eb80e86653c98386eb
Hrvoje Krot <hkrot@infoartgroup.com>
2018-01-12 07:53:10 +0100

    Adding some code.

--- src/paip/prolog1.clj
+++ src/paip/prolog1.clj
@@ -1,3 +1,21 @@
 (ns ^{:doc "First version of the prolog interpreter (11.2)."}
   paip.prolog1
-  (:require [paip.unify :refer :all]))
+  (:require [paip.unify :refer :all]
+            [paip.auxfns :refer (variable?)]))
+
+(defn clause-head [clause] (first clause))
+(defn clause-body [clause] (rest clause))
+
+(def clauses {})
+
+(def db-predicates '())
+
+(defn get-clauses [pred] (clauses pred))
+(defn predicate [relation] (first relation))
+
+(defn add-clause
+  [clause]
+  (let [pred (predicate (clause-head clause))]
+    (assert (and (symbol? pred) (not (variable? pred))))
+    ))
+
