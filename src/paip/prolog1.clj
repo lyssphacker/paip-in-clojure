5db28723683d8a2234a8f0c24cc3d4f1a769b1d1
Hrvoje Krot <hkrot@infoartgroup.com>
2018-01-12 16:36:27 +0100

    Adding code for adding clauses.

--- src/paip/prolog1.clj
+++ src/paip/prolog1.clj
@@ -1,7 +1,7 @@
 (ns ^{:doc "First version of the prolog interpreter (11.2)."}
   paip.prolog1
   (:require [paip.unify :refer :all]
-            [paip.auxfns :refer (variable?)]))
+            [paip.auxfns :refer (variable? prepend)]))
 
 (defn clause-head [clause] (first clause))
 (defn clause-body [clause] (rest clause))
@@ -17,5 +17,15 @@
   [clause]
   (let [pred (predicate (clause-head clause))]
     (assert (and (symbol? pred) (not (variable? pred))))
-    ))
+    (alter-var-root
+      #'db-predicates
+          (fn [p]
+            (prepend pred p)))
+    (alter-var-root
+      #'clauses
+      (fn [m]
+        (assoc m pred (concat (pred m) (list clause)))))
+    pred))
 
+(defmacro <- [& clause]
+  `(add-clause '~clause))
