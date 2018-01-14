d00ea2946c0cb33a642b4024b5d2a1c89ee0418a
Hrvoje Krot <hrvoje.krot@gmail.com>
2018-01-14 18:07:28 +0100

    Adding some more functions.

--- src/paip/prolog.clj
+++ src/paip/prolog.clj
@@ -6,7 +6,9 @@
                                              clear-predicate unique-find-anywhere-if
                                              clauses rename-variables)]
             [paip.unify :refer :all]
-            [paip.auxfns :refer (funcall fail)]))
+            [paip.auxfns :refer (funcall fail no-bindings)]
+            [clojure.pprint :refer (cl-format)]
+            [clojure.walk :refer (postwalk-replace)]))
 
 (declare prove-all)
 
@@ -28,3 +30,33 @@
   (cond (= bindings fail) fail
         (empty? goals) bindings
         :else (prove (first goals) bindings (rest goals))))
+
+(defn continue?
+  []
+  (let [input (read)]
+    (cond (= input 't ) true
+          (= input 'f) nil
+          (= input 'c) (continue?)
+          :else (do
+                  (cl-format true " Type ; to see more or . to stop")
+                  (continue?)))))
+
+(defn show-prolog-vars
+  [vars bindings other-goals]
+  (if (empty? vars)
+    (cl-format true "~&Yes~%")
+    (doseq [var vars]
+      (cl-format true "~&~a = ~a~%" var
+              (postwalk-replace bindings var))))
+  (if (continue?)
+    fail
+    (prove-all other-goals bindings)))
+
+(declare variables-in)
+
+(defn top-level-prove
+  [goals]
+  (prove-all `(~@goals (show-prolog-vars ~@(variables-in goals)))
+             no-bindings)
+  (cl-format true "~&No.~%"))
+
