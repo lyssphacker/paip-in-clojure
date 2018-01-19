9456fca6aa65b7cc22a91d10596a19feea5161ab
Hrvoje Krot <hrvoje.krot@gmail.com>
2018-01-19 20:02:30 +0100

    Fixing doc.

--- src/paip/prologc1.clj
+++ src/paip/prologc1.clj
@@ -1,3 +1,3 @@
-(ns ^{:doc "Version 1 of the prolog compiler, \n;;;; including the destructive unification routines from Chapter 11."}
+(ns ^{:doc "Version 1 of the prolog compiler, including the destructive unification routines from Chapter 11."}
   paip.prologc1
   (:require [paip.prolog :refer :all]))
