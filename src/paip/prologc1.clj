b11cd4f837a359aad25cac526db50c5949ed987e
Hrvoje Krot <hrvoje.krot@gmail.com>
2018-01-21 14:08:20 +0100

    Adding some code.

--- src/paip/prologc1.clj
+++ src/paip/prologc1.clj
@@ -1,3 +1,20 @@
 (ns ^{:doc "Version 1 of the prolog compiler, including the destructive unification routines from Chapter 11."}
   paip.prologc1
-  (:require [paip.prolog :refer :all]))
+  (:require [paip.prolog :refer :all]
+            [paip.auxfns :refer (defrecord+)]))
+
+(def unbound "Unbound")
+
+(defrecord+ variable [name [binding unbound]])
+
+(defn bound-var?
+  [var]
+  (not= (:binding var) unbound))
+
+(defmacro deref-var
+  "Follow pointers for bound variables."
+  [exp]
+  `(loop [exp# ~exp]
+     (if (and (variable? exp#) (bound-var? exp#))
+       (recur (:binding exp#))
+       exp#)))
